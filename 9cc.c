8912a3b278595af695c9ecc11fa92dd8849fb29a
Arisa Okamoto <qslyuu@gmail.com>
2023-12-26 18:35:42 +0900

    エラーメッセージを改良

--- 9cc.c
+++ 9cc.c
@@ -30,6 +30,21 @@ void error(char* fmt, ...) {
   exit(1);
 }
 
+char* user_input;
+
+void error_at(char* loc, char* fmt, ...) {
+  va_list ap;
+  va_start(ap, fmt);
+
+  int pos = loc - user_input;
+  fprintf(stderr, "%s\n", user_input);
+  fprintf(stderr, "%*s", pos, " ");
+  fprintf(stderr, "^ ");
+  vfprintf(stderr, fmt, ap);
+  fprintf(stderr, "\n");
+  exit(1);
+}
+
 bool consume(char op) {
   if (token->kind != TK_RESERVED || token->str[0] != op) {
     return false;
@@ -40,14 +55,14 @@ bool consume(char op) {
 
 void expect(char op) {
   if (token->kind != TK_RESERVED || token->str[0] != op) {
-    error("'%c'ではありません", op);
+    error_at(token->str, "'%c'ではありません", op);
   }
   token = token->next;
 }
 
 int expect_number() {
   if (token->kind != TK_NUM) {
-    error("数字ではありません");
+    error_at(token->str, "数字ではありません");
   }
   int val = token->val;
   token = token->next;
@@ -88,7 +103,7 @@ Token* tokenize(char* p) {
       continue;
     }
 
-    error("トークナイズできません");
+    error_at(token->str, "トークナイズできません");
   }
 
   new_token(TK_EOF, cur, p);
@@ -101,6 +116,7 @@ int main(int argc, char** argv) {
     return 1;
   }
 
+  user_input = argv[1];
   token = tokenize(argv[1]);
 
   printf(".intel_syntax noprefix\n");
